2d59f7c9ff7e74008fff6b2d93bba3e24fdf269e
Michael Birkert <birkert.michael@gmail.com>
2026-05-31 17:26:22 -0500

    add ce course schemas

---
+++ backend/app/schemas/ce_course.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, date
+
+class CECourseCreate(BaseModel):
+    course_name : str
+    hours_earned : int
+    date_completed : date
+    course_provider : str
+    
+    
+class CECourseResponse(BaseModel):
+    id : int
+    course_name : str
+    date_completed : date
+    certificate_url : Optional[str] = None
+    course_provider :str 
+    created_at : datetime
+    hours_earned : int
+    
+    class Config:
+        from_attributes = True
+        
+        
+class CECourseUpdate(BaseModel):
+    course_name : Optional[str] = None
+    date_completed : Optional[date] = None
+    certificate_url : Optional[str] = None
