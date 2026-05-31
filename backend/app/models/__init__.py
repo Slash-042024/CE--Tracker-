143c1ff9b72678fbe26cfb2daebf48c3aae83e7a
Michael Birkert <birkert.michael@gmail.com>
2026-05-31 16:08:14 -0500

    register all models in init

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.user import User
+from app.models.license_holder import LicenseHolder
+from app.models.license import License
+from app.models.ce_course import CECourse
+from app.models.alert_log import AlertLog
+from app.models.subscription import Subscription
